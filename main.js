5fc750e47f057ef9369718b3537246fbd9cb3f35
Collin Rogowski <collin.rogowski@inovex.de>
2021-01-07 15:59:51 +0100

    clear stickers

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ miro.onReady(() => {
   miro.initialize({
     extensionPoints: {
       bottomBar: {
-        title: 'Sticker to shapes',
+        title: 'Throw Dice',
         svgIcon:
           '<circle cx="12" cy="12" r="9" fill="none" fill-rule="evenodd" stroke="currentColor" stroke-width="2"/>',
         positionPriority: 1,
@@ -14,8 +14,9 @@ miro.onReady(() => {
           let stickers = selectedWidgets.filter((widget) => widget.type === 'STICKER')
 
           // Delete selected stickers
-          await miro.board.widgets.deleteById(stickers.map((sticker) => sticker.id))
+          await stickers.map((sticker) => sticker.text = "")
 
+          /* 
           // Create shapes from selected stickers
           await miro.board.widgets.create(
             stickers.map((sticker) => ({
@@ -27,6 +28,7 @@ miro.onReady(() => {
               height: sticker.bounds.height,
             })),
           )
+          */
 
           // Show success message
           miro.showNotification('Stickers has been converted')
